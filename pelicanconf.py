8dce81471fa86be731ae5a4d3ec85fbad2180219
Sitala <titan2169@yandex.ru>
2022-12-28 18:02:16 +0200

    sitemap

--- pelicanconf.py
+++ pelicanconf.py
@@ -43,16 +43,29 @@ RFG_FAVICONS = True
 SITESUBTITLE = 'A magical \u2728 Pelican theme'
 SITEIMAGE = '/images/1087sm-700x700.png width=300 height=300'
 
-
 PYGMENTS_STYLE = 'autumn'
 
 THEME_JS_OVERRIDES = ['theme/js/bootstrap.min.js',]
 BOOTSTRAP_CSS = 'theme/css/bootstrap.min.css'
 
-PLUGINS = ['plugins.bootstrap', 'render_math']
+PLUGINS = ['plugins.bootstrap', 'render_math', "sitemap"]
 
 BOOTSTRAPIFY = {
     'table': ['table', 'table-striped', 'table-hover'],
     'img': ['img-fluid', "img-thumbnail"],
     'blockquote': ['blockquote'],
+}
+
+SITEMAP = {
+    "format": "xml",
+    "priorities": {
+        "articles": 0.5,
+        "indexes": 0.5,
+        "pages": 0.5
+    },
+    "changefreqs": {
+        "articles": "monthly",
+        "indexes": "daily",
+        "pages": "monthly"
+    }
 }

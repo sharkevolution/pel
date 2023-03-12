febad0157834ebbb74c6404b05d49211454a9773
Sitala <titan2169@yandex.ru>
2023-03-13 00:59:05 +0200

    update

--- alchemy/static/js/scrollup.js
+++ alchemy/static/js/scrollup.js
@@ -1,30 +1,24 @@
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
 
-// Get the button
-	let mybutton = document.getElementById("myBtn");
-  
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  // Get the button
+  let mybutton = document.getElementById("myBtn");
+
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-//function topFunction() {
-//  document.body.scrollTop = 0;
-//  document.documentElement.scrollTop = 0;
-//}
-
 function topFunction() {
-	var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-  if(top > 0) {
-	window.scrollBy(0,((top+100)/-10));
-	t = setTimeout('topFunction()',20);
+  var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+  if (top > 0) {
+    window.scrollBy(0, ((top + 100) / -10));
+    t = setTimeout('topFunction()', 20);
   } else clearTimeout(t);
   return false;
 }

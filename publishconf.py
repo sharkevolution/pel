c39e3a9ac9f7ddbe8a2cd31890ba0ab8475a0a8b
Sitala <titan2169@yandex.ru>
2023-05-29 14:49:34 +0300

    rss

--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,11 @@ from pelicanconf import *
 SITEURL = 'https://sitala.netlify.app'
 RELATIVE_URLS = False
 
+# Feed Items
+FEED_MAX_ITEMS = 15
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+
 
 DELETE_OUTPUT_DIRECTORY = True
 
